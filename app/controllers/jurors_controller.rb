3ba00db3f121e66bb431de8013094df617d47c24
Geoffrey Arner <geoffarner5@gmail.com>
2022-08-12 16:43:02 -0400

    Added index for DB

--- app/controllers/jurors_controller.rb
+++ app/controllers/jurors_controller.rb
@@ -1,2 +1,6 @@
 class JurorsController < ApplicationController
+  def index
+    jurors = Juror.all
+    render json: jurors.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #jurors index
+  get "/jurors" => "jurors#index"
+  #users index
+  get "/users" => "users#index"
 end
