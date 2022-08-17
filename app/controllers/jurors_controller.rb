11b286ba5948daa8c62600984bb82f60dab7451f
Geoffrey Arner <geoffarner5@gmail.com>
2022-08-17 11:11:12 -0500

    Made Juror Actions Index, Show, Create, Update, Destroy almost...

--- app/controllers/jurors_controller.rb
+++ app/controllers/jurors_controller.rb
@@ -11,6 +11,34 @@ class JurorsController < ApplicationController
     render :show
   end
 
+  def create
+    juror = Juror.new(
+      panel_number: params[:panel_number],
+      name: params[:name],
+      age: params[:age],
+      sex: params[:sex],
+      residence: params[:residence],
+      housing: params[:housing],
+      education: params[:password],
+      law_experience: params[:law_experience],
+      occupation: params[:occupation],
+      military_service: params[:password],
+      jury_relation: params[:jury_relation],
+      prior_jury: params[:prior_jury],
+      notes: params[:notes],
+      jury_pool: params[:jury_pool],
+      jury_box: params[:jury_box],
+      jury_alternate: params[:jury_alternate],
+
+    )
+    if juror.save
+      render json: { message: "Juror created successfully" }, status: :created
+    else
+      render json: { errors: juror.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  ### FIX UPDATE ###
   def update
     @juror = Juror.find_by(id: params[:id])
     @juror.jury_pool = params[:jury_pool] || @juror.jury_pool
