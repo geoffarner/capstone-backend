3ce5c93a91a9155c8c135f19fc40e7d2b438889e
Geoffrey Arner <geoffarner5@gmail.com>
2022-08-19 13:43:45 -0500

    Fixed Boolean, Update, and Create

--- app/controllers/jurors_controller.rb
+++ app/controllers/jurors_controller.rb
@@ -12,17 +12,17 @@ class JurorsController < ApplicationController
   end
 
   def create
-    juror = Juror.new(
+    @juror = Juror.new(
       panel_number: params[:panel_number],
       name: params[:name],
       age: params[:age],
       sex: params[:sex],
       residence: params[:residence],
       housing: params[:housing],
-      education: params[:password],
+      education: params[:education],
       law_experience: params[:law_experience],
       occupation: params[:occupation],
-      military_service: params[:password],
+      military_service: params[:military_service],
       jury_relation: params[:jury_relation],
       prior_jury: params[:prior_jury],
       notes: params[:notes],
@@ -31,25 +31,36 @@ class JurorsController < ApplicationController
       jury_alternate: params[:jury_alternate],
 
     )
-    if juror.save
-      render json: { message: "Juror created successfully" }, status: :created
+    if @juror.save
+      render :show, status: :created
     else
       render json: { errors: juror.errors.full_messages }, status: :bad_request
     end
   end
 
-  ### FIX UPDATE ###
   def update
     @juror = Juror.find_by(id: params[:id])
-    @juror.jury_pool = params[:jury_pool] || @juror.jury_pool
-    @juror.jury_box = params[:jury_box] || @juror.jury_box
-    @juror.jury_alternate = params[:jury_alternate] || @juror.jury_alternate
+    @juror.panel_number = params[:panel_number] || @juror.panel_number
+    @juror.name = params[:name] || @juror.name
+    @juror.age = params[:age] || @juror.age
+    @juror.sex = params[:sex] || @juror.sex
+    @juror.residence = params[:residence] || @juror.residence
+    @juror.housing = params[:housing] || @juror.housing
+    @juror.education = params[:education] || @juror.education
+    @juror.law_experience = params[:law_experience] || false
+    @juror.occupation = params[:occupation] || @juror.occupation
+    @juror.military_service = params[:military_service] || false
+    @juror.jury_relation = params[:jury_relation] || false
+    @juror.prior_jury = params[:prior_jury] || @juror.prior_jury
+    @juror.notes = params[:notes] || @juror.notes
+    @juror.jury_pool = params[:jury_pool] || false
+    @juror.jury_box = params[:jury_box] || false
+    @juror.jury_alternate = params[:jury_alternate] || false
 
     if @juror.save
       render :show
     else
-      render json: { errors: @juror.errors.full_messages },
-             status: 401
+      render json: { errors: @juror.errors.full_messages }, status: 401
     end
   end
 
